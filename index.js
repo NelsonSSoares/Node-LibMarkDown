6a77629abfe36a3856dc2b25fbd8fbbe02de058e
Nelson <punkpunkada@gmail.com>
2022-03-11 15:04:56 -0300

    fs.promisses

--- index.js
+++ index.js
@@ -1,18 +1,31 @@
+// chalk, lib para colorir textos de console.. debug
 const chalk = require('chalk');
-const { Console } = require('console');
 const fs = require('fs');
 
-function tratarError(error){
+function tratarError(error) {
   throw new Error(chalk.red(error.code, 'Arquivo Não encontrado!'))
 }
 
 
-function pegaArquivos(path){
+function pegarArquivos(path){
+  const encoding = 'utf-8';
+  fs.promises.readFile(path,encoding)
+  .then((text)=>{ 
+    console.log(chalk.green(text))
+  })
+  .catch((error)=>{
+    tratarError(error)
+  })
+}
+
+pegarArquivos('./arquivos/texto1.md');
+/* 
+function pegaArquivos(path) {
   const encoding = 'utf-8'
 
   // _ FAZ IGNORAR PARAMETRO DA FUNÇÃO
-  fs.readFile(path, encoding,(error, data) => {
-    if(error){
+  fs.readFile(path, encoding, (error, data) => {
+    if (error) {
       tratarError(error)
     }
     console.log(chalk.green(data));
@@ -20,4 +33,4 @@ function pegaArquivos(path){
 
 }
 
-pegaArquivos('./arquivos/texto1.md');
+pegaArquivos('./arquivos/texto1.md'); */
