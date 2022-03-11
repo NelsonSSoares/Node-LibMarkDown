859a474474614dfbf06b5fc1639a91ac6a91a11f
Nelson <punkpunkada@gmail.com>
2022-03-11 13:26:44 -0300

    tratamento de errors

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
 const chalk = require('chalk');
+const { Console } = require('console');
+const fs = require('fs');
 
-console.log(chalk.blue('vamos começar!'));
-
-const paragrafo = 'Texto retornado por uma função';
-
-function texto(string) {
-  return string;
+function tratarError(error){
+  throw new Error(chalk.red(error.code, 'Arquivo Não encontrado!'))
 }
 
-console.log(texto(paragrafo));
+
+function pegaArquivos(path){
+  const encoding = 'utf-8'
+
+  // _ FAZ IGNORAR PARAMETRO DA FUNÇÃO
+  fs.readFile(path, encoding,(error, data) => {
+    if(error){
+      tratarError(error)
+    }
+    console.log(chalk.green(data));
+  })
+
+}
+
+pegaArquivos('./arquivos/texto1.md');
