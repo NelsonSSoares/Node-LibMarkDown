b6fd1fb978398c751f9c15da33b68e15edb7dc1d
Nelson <punkpunkada@gmail.com>
2022-03-12 13:05:26 -0300

    REGEX

--- index.js
+++ index.js
@@ -2,6 +2,19 @@
 const chalk = require('chalk');
 const fs = require('fs');
 
+//REGEX CAP LINK
+/*\[([^\]]*)\]\((https?:\/\/[^$#\s].[^\s]*)\)*/
+
+const texto = 'São geralmente recuperados a partir de um objeto [FileList](https://developer.mozilla.org/pt-BR/docs/Web/API/FileList) que é retornado como resultado da seleção, pelo usuário, de arquivos através do elemento [<input>](https://developer.mozilla.org/pt-BR/docs/Web/HTML/Element/Input), a partir do objeto [DataTransfer](https://developer.mozilla.org/pt-BR/docs/Web/API/DataTransfer) utilizado em operações de arrastar e soltar, ou a partir da API `mozGetAsFile()` em um [HTMLCanvasElement](https://developer.mozilla.org/pt-BR/docs/Web/API/HTMLCanvasElement). Em Gecko, códigos com privilégiios podem criar objetos File representando qualquer arquivo local sem a intereção do usuário (veja [Implementation notes](https://developer.mozilla.org/pt-BR/docs/Web/API/File#implementation_notes) para mais informações.)'
+
+function extraiLinks(texto){
+  const regex = /\[([^\]]*)\]\((https?:\/\/[^$#\s].[^\s]*)\)/gm;
+  const linksExtraidos = texto.match(regex);
+  console.log(chalk.green(linksExtraidos));
+}
+
+
+  // _ FAZ IGNORAR PARAMETRO DA FUNÇÃO
 function tratarError(error) {
   throw new Error(chalk.red(error.code, 'Arquivo Não encontrado!'))
 }
@@ -18,8 +31,25 @@ async function pegarArquivos(path){
   }
   
 }
+extraiLinks(texto)
+//pegarArquivos('./arquivos/texto1.md');
 
-pegarArquivos('./arquivos/texto1.md');
+/* [
+  {
+    [nomeDoLink]: (url)
+  
+  }
+]
+ */
+
+
+
+
+
+
+
+
+
 
 /* 
 function pegarArquivos(path){
