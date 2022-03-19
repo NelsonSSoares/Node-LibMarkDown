8703d1bc15d776536119f6c1bad6f13b6b4328e6
Nelson <punkpunkada@gmail.com>
2022-03-19 13:39:15 -0300

    capturandp mais de uma correncia REGEX

--- index.js
+++ index.js
@@ -1,16 +1,23 @@
 // chalk, lib para colorir textos de console.. debug
 const chalk = require('chalk');
+const { exec } = require('child_process');
 const fs = require('fs');
 
 //REGEX CAP LINK
 /*\[([^\]]*)\]\((https?:\/\/[^$#\s].[^\s]*)\)*/
 
-const texto = 'São geralmente recuperados a partir de um objeto [FileList](https://developer.mozilla.org/pt-BR/docs/Web/API/FileList) que é retornado como resultado da seleção, pelo usuário, de arquivos através do elemento [<input>](https://developer.mozilla.org/pt-BR/docs/Web/HTML/Element/Input), a partir do objeto [DataTransfer](https://developer.mozilla.org/pt-BR/docs/Web/API/DataTransfer) utilizado em operações de arrastar e soltar, ou a partir da API `mozGetAsFile()` em um [HTMLCanvasElement](https://developer.mozilla.org/pt-BR/docs/Web/API/HTMLCanvasElement). Em Gecko, códigos com privilégiios podem criar objetos File representando qualquer arquivo local sem a intereção do usuário (veja [Implementation notes](https://developer.mozilla.org/pt-BR/docs/Web/API/File#implementation_notes) para mais informações.)'
+
 
 function extraiLinks(texto){
   const regex = /\[([^\]]*)\]\((https?:\/\/[^$#\s].[^\s]*)\)/gm;
-  const linksExtraidos = texto.match(regex);
-  console.log(chalk.green(linksExtraidos));
+  const arrayResultados = [];
+  let temp;
+
+  while((temp = regex.exec(texto)) !== null){
+    arrayResultados.push({[temp[1]]: temp[2]})
+  }
+  return arrayResultados
+  //console.log(chalk.green(arrayResultados));
 }
 
 
@@ -25,14 +32,14 @@ async function pegarArquivos(path){
   
   try {
     const texto = await fs.promises.readFile(path,encoding);
-    console.log(chalk.green(texto));
+    console.log(extraiLinks(texto));
   } catch (error) {
     tratarError(error)
   }
   
 }
-extraiLinks(texto)
-//pegarArquivos('./arquivos/texto1.md');
+
+pegarArquivos('./arquivos/texto1.md');
 
 /* [
   {
