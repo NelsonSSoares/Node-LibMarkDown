8773ad843750e8f60d77359532ab63ed913d0632
Nelson <punkpunkada@gmail.com>
2022-03-28 13:21:59 -0300

    refatorando

--- cli.js
+++ cli.js
@@ -7,7 +7,7 @@ const caminho = process.argv;
 async function processaTexto(caminhoDeArquivo){
     const resultado = await pegaArquivos(caminhoDeArquivo[2])
     if(caminho[3] === 'validar'){
-        console.log(chalk.yellow('Links Validados'), validaURLs(resultado));
+        console.log(chalk.yellow('Links Validados'), await validaURLs(resultado));
     }else{
         console.log(chalk.yellow('lista de Links'), resultado)
     }
--- http-validacao.js
+++ http-validacao.js
@@ -1,5 +1,24 @@
-function validaURLs(arrayLink){
-    return arrayLink
+const fetch = require('node-fetch');
+
+
+async function checaStatus(arrayURLs) {
+    const arrayStatus = await Promise
+        .all(arrayURLs.map(async url => {
+            const res = await fetch(url)
+            return res.status;
+        }))
+    return arrayStatus
+}
+
+function geraArrayURL(arrayLink) {
+    return arrayLink.map(objetoLink => Object.values(objetoLink).join())
+}
+
+async function validaURLs(arrayLink) {
+    const links = geraArrayURL(arrayLink)
+    const statusLinks = await checaStatus(links);
+    console.log(arrayLink, statusLinks);
+    return statusLinks;
 }
 
 module.exports = validaURLs;
