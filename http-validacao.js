40a938c0439393738037282f44da87fcb66c530c
Nelson <punkpunkada@gmail.com>
2022-03-29 19:11:38 -0300

    jest testando aplicação

--- http-validacao.js
+++ http-validacao.js
@@ -1,17 +1,17 @@
 const fetch = require('node-fetch');
 
-function manejaError(error){
+function manejaError(error) {
     throw new Error(error.message);
 }
 
 async function checaStatus(arrayURLs) {
     try {
         const arrayStatus = await Promise
-        .all(arrayURLs.map(async url => {
-            const res = await fetch(url)
-            return res.status;
-        }))
-    return arrayStatus
+            .all(arrayURLs.map(async url => {
+                const res = await fetch(url)
+                return res.status;
+            }))
+        return arrayStatus
     } catch (error) {
         manejaError(error)
     }
@@ -24,7 +24,7 @@ function geraArrayURL(arrayLink) {
 async function validaURLs(arrayLink) {
     const links = geraArrayURL(arrayLink)
     const statusLinks = await checaStatus(links);
-    
+
     const resultados = arrayLink.map((objeto, index) => ({
         ...objeto, status: statusLinks[index]
     }))
---
+++ test/index.test.js
@@ -0,0 +1,7 @@
+const pegaArquivo = require('../index');
+
+// teste para ver se é uma função
+test('deve ser uma função', () => {
+
+    expect(typeof pegaArquivo).toBe('function');
+  });
