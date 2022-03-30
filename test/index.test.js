f42a4ca4fa62bbe7e488ed22ac089da42efbad2c
Nelson <punkpunkada@gmail.com>
2022-03-30 16:49:13 -0300

    Jest: testando aplicação

--- test/index.test.js
+++ test/index.test.js
@@ -1,7 +1,33 @@
 const pegaArquivo = require('../index');
 
-// teste para ver se é uma função
+const arrayResult = [
+  {
+    FileList: 'https://developer.mozilla.org/pt-BR/docs/Web/API/FileList'
+  }
+]
+
+describe('pegaArquivo::',()=>{
+
+  it('deve ser uma função', ()=>{
+    expect(typeof pegaArquivo).toBe('function');
+  })
+
+
+  it('Deve retornar Array com resultados', async () =>{
+    const result = await pegaArquivo('.\test\files\texto1.md')
+    expect(result).toEqual(arrayResult)
+  }) 
+  
+})
+
+
+
+
+
+
+
+/* // teste para ver se é uma função
 test('deve ser uma função', () => {
 
     expect(typeof pegaArquivo).toBe('function');
-  });
+  }); */
